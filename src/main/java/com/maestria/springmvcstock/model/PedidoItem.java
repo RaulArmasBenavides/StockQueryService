8e3d5560fb478b7e2f3bbc0d2da7316b223c7f58
Raúl Armas <raularmasbx@gmail.com>
2024-06-03 01:41:11 -0500

    RMAB updates

---
+++ src/main/java/com/maestria/springmvcstock/model/PedidoItem.java
@@ -0,0 +1,19 @@
+package com.maestria.springmvcstock.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+@Entity
+@Data
+public class PedidoItem {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @ManyToOne
+    private Producto producto;
+    private int cantidad;
+
+    // Getters y setters
+}
